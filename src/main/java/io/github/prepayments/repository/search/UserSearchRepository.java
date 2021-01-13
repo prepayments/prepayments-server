79e408a9d966f0e2d0fda27430d748fe60ddb121
Edwin Njeru <mailnjeru@gmail.com>
2021-01-13 11:35:25 +0300

    Initial version of prepayments generated by JHipster-6.10.5

---
+++ src/main/java/io/github/prepayments/repository/search/UserSearchRepository.java
@@ -0,0 +1,11 @@
+package io.github.prepayments.repository.search;
+
+import io.github.prepayments.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
