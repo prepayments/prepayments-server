84090232feeb8d0ce9a275c9c5e2e51f2220801f
Edwin Njeru <mailnjeru@gmail.com>
2021-01-21 21:30:31 +0300

    Fix missing prepayment amount

--- src/main/java/io/github/prepayments/internal/model/PrepaymentDataEntryDTOMapping.java
+++ src/main/java/io/github/prepayments/internal/model/PrepaymentDataEntryDTOMapping.java
@@ -3,9 +3,21 @@ package io.github.prepayments.internal.model;
 import io.github.prepayments.internal.Mapping;
 import io.github.prepayments.service.dto.PrepaymentDataDTO;
 import io.github.prepayments.service.dto.PrepaymentEntryDTO;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mappings;
 
-@Mapper(componentModel = "spring", uses = {})
+import java.math.BigDecimal;
+
+
 public interface PrepaymentDataEntryDTOMapping extends Mapping<PrepaymentDataDTO, PrepaymentEntryDTO> {
 
+    @Mappings(
+        value = {
+            @org.mapstruct.Mapping(target = "transactionAmount", source = "prepaymentAmount"),
+        }
+    )
+    default BigDecimal toBigDecimalMap(BigDecimal doublePrecisionAmount) {
+        return doublePrecisionAmount;
+    }
 }
---
+++ src/main/java/io/github/prepayments/internal/model/PrepaymentDataEntryMap.java
@@ -0,0 +1,50 @@
+package io.github.prepayments.internal.model;
+
+import io.github.prepayments.service.dto.PrepaymentDataDTO;
+import io.github.prepayments.service.dto.PrepaymentEntryDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PrepaymentDataEntryMap implements PrepaymentDataEntryDTOMapping  {
+
+
+    @Override
+    public PrepaymentDataDTO toValue1(PrepaymentEntryDTO arg0) {
+        if ( arg0 == null ) {
+            return null;
+        }
+
+        PrepaymentDataDTO prepaymentDataDTO = new PrepaymentDataDTO();
+
+        prepaymentDataDTO.setId( arg0.getId() );
+        prepaymentDataDTO.setAccountName( arg0.getAccountName() );
+        prepaymentDataDTO.setDescription( arg0.getDescription() );
+        prepaymentDataDTO.setAccountNumber( arg0.getAccountNumber() );
+        prepaymentDataDTO.setPrepaymentNumber( arg0.getPrepaymentNumber() );
+        prepaymentDataDTO.setPrepaymentDate( arg0.getPrepaymentDate() );
+        prepaymentDataDTO.setUploadToken( arg0.getUploadToken() );
+        prepaymentDataDTO.setPrepaymentAmount(arg0.getTransactionAmount());
+
+        return prepaymentDataDTO;
+    }
+
+    @Override
+    public PrepaymentEntryDTO toValue2(PrepaymentDataDTO arg0) {
+        if ( arg0 == null ) {
+            return null;
+        }
+
+        PrepaymentEntryDTO prepaymentEntryDTO = new PrepaymentEntryDTO();
+
+        prepaymentEntryDTO.setId( arg0.getId() );
+        prepaymentEntryDTO.setAccountName( arg0.getAccountName() );
+        prepaymentEntryDTO.setDescription( arg0.getDescription() );
+        prepaymentEntryDTO.setAccountNumber( arg0.getAccountNumber() );
+        prepaymentEntryDTO.setPrepaymentNumber( arg0.getPrepaymentNumber() );
+        prepaymentEntryDTO.setPrepaymentDate( arg0.getPrepaymentDate() );
+        prepaymentEntryDTO.setUploadToken( arg0.getUploadToken() );
+        prepaymentEntryDTO.setTransactionAmount(arg0.getPrepaymentAmount());
+
+        return prepaymentEntryDTO;
+    }
+}
